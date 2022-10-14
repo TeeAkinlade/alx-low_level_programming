557951b1d3ca87a7c4b9a45b8473fec899f7d99a
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-10-14 14:12:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrt point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
