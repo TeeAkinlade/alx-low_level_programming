59141012e997aff4a02cbf1d884f9da83774eda1
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-10-14 14:48:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,16 @@ int main(void)
 
 	c ='a';
 	d = 'A';
-	while (c <='z'){
+	while (c <= 'z'
+	){
 		putchar(c);
 		c++;
 	}
-	while (d <='Z'){
+	while (d <= 'Z')
+	{
 		putchar(d);
 		d++;
 	}
 	putchar('\n');
-	return(0);
+	retur n(0);
 }
