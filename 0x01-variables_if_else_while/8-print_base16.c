49c970bc71ca70bc4835b5b4c9f67f296f342bad
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-10-17 15:49:22 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,6 +2,7 @@
 /**
  * main - Entry point
  *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
@@ -16,7 +17,7 @@ int main(void)
 			putchar(d + '0');
 			d++;
 		}
-	while 
+	while
 		(c <= 'f') {
 			putchar(c);
 			c++;
