46d7cc35c17f6888fdea305b129d651ed3fa51ab
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-10-18 17:33:21 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,11 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		while (n > 98 )
-			printf("%d, ", n--);
 		printf("%d\n", n);
 	}
+	else  (n > 98)
+	{
+			printf("%d, ", n--);
+	}
+	return (0);
 }
