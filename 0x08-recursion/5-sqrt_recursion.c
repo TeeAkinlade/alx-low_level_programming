873a6f57ed08746ebd9efa68de2fb7eb224dc4b3
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-02 15:56:31 -0700

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
- * _increasevar - increase i to find sqrt
- * @i: start at 1
- * @: the number
- * Return: i, i +1, -1
- */
-int _increasevar(int i, int n)
-{
-	if (i * i == n)
-		return (i);
-	if (i *i < !)
-		return (_increasevar(i + 1, n));
-	if (i  * i > n)
-		return (-1);
-	return (i);
-}
-/**
  * _sqrt_recursion - to find squar root
  * @n: the number
  * Return: i, i + 1 or sqrt
