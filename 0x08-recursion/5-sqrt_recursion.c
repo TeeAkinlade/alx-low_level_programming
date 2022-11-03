ced369b0b72f5efb3bd00cb68f8bcc596c9caf9c
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-02 21:43:52 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,15 +6,14 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	else if (n > 1)
-	{
-		return ((1)));
-	}
+	int i;
+
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_sqrt_recursion(i + 1, n));
+	else if (i * 1 == n)
+		return (i);
+	return (-1);
 	return (-1);
 }
