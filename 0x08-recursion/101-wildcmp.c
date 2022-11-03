92d8b7abb8933a01c9a0166fad55fd13a37ca97e
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-03 01:51:35 -0700

    task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
- /**
+/**
   * wildcmp - checks two strings for match even if one is full of wildcard
   * @s1: firat string
   * @s2: second strg
   * Return: 1 ,other 0
-  */
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2 && *s1 && *s2)
