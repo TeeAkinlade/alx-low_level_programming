244eeff88fe6620d9498c23844537e9511b50591
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-08 12:24:40 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,24 +2,6 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strlen - count and return string
- * @s: string
- * Return: On success return 1
- */
-int _strlen(char *s)
-{
-	int counter = 0;
-
-	if (!*s)
-		return (0);
-	while (*s)
-	{
-		counter++;
-		s++;
-	}
-	return (counter);
-}
-/**
  * str_concat - malloc to concatinate two strings
  * @s1: first string
  * @s2: second string
@@ -27,30 +9,26 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *new;
-	unsigned int i;
-	unsigned int j;
-	int total = 0;
+	char *concat_str;
+	int s1_size = 0, s2_size = 0, counter = 0, counter2 = 0;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
-	total += _strlen(s1) + _strlen(s2);
-	new = malloc((total * sizeof(char)) + 1);
-	if (new == NULL)
+	while (*(s1 + s1_size) != '\0')
+		s1_size++;
+	while (*(s2 + s2_size) != '\0')
+		s2_size++;
+	concat_str = malloc((s1_size + s2_size + 1) * sizeof(char));
+	if (concat_str == NULL)
 	{
-		return (NULL);
+		concat_str[counter] = s1[counter];
 	}
-	for (i = 0; s1[i]; i++)
+	for (counter2 = 0; s2[counter2] != '\0'; counter++, counter2++)
 	{
-		new[i] = s1[i];
+		concat_str[counter] = s2[counter2];
 	}
-	for (j = 0; s1[2]; j++, i++)
-	{
-		new[i] = s2[j];
-	}
-	new[i] = '\0';
-	return (new);
+	concat_str[counter] = '\0';
+	return (concat_str);
 }
-
