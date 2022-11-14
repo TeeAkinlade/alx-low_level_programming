3237791ad5845b3faff1e1a731b49bfabee246c6
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-14 09:56:49 -0800

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - check the code
  * Return: on success return 0
  * _FILE_: returns the current file we are working on
  */
-int main(void)
-
-	printf("%s\n", _FILE_);
-		return (0);
+int main()
+{
+	printf("Name of source file: %s\n", __FILE__);
+		return 0;
 }
