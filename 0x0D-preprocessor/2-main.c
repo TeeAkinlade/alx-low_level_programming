a6adbdcc7f15dadcd378c4d226c8efc7ea1a8a73
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-14 09:34:29 -0800

    task 4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,9 +2,10 @@
 /**
  * main - check the code
  * Return: on success return 0
+ * _FILE_: returns the current file we are working on
  */
 int main(void)
-{
+
 	printf("%s\n", _FILE_);
 		return (0);
 }
