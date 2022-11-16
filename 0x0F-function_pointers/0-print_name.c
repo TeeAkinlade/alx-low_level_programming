30381149a1bd603a653b9e0029974bb26f43d684
TeeAkinlade <taiye.akinlade1@gmail.com>
2022-11-16 12:48:54 -0800

    tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,6 @@
  * print_name - a program that print name
  * @name: the name to be printed
  * @f: pointer to the printing function
- * Return: on success 1
  */
 void print_name(char *name, void (*f)(char *))
 {
